48ffb2fc78ffdd508a9f055f26b78947c965a877
shriharikallapur <shriharikallapur369@gmail.com>
2024-03-03 12:40:23 +0530

    update home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 export const Home = () => {
+  if(window.location.toString().includes("/")) {
+    document.getElementById('topbar').style.display = 'none'
+    document.getElementById('sidebar').style.display = 'none'
+  }
   return (
-    <div>Home</div>
+    <></>
   )
 }
